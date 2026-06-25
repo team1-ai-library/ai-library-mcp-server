5eb13b354bd6eb07ec69d8934b8747e90b35da20
SRIOUSS <changhui5796@gmail.com>
2026-06-25 16:34:40 +0900

    first: commit

---
+++ src/main/java/com/nhnacademy/data4library/Data4libraryApplication.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.data4library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Data4libraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Data4libraryApplication.class, args);
+    }
+
+}
