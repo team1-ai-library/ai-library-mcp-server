0078af6ee9c7cd87d138f1e4f67db9db05a0a79b
woalshue <sonjaemin33@gmail.com>
2026-06-27 17:15:31 +0900

    feat: McpConfig, yaml 설정

--- src/main/java/com/nhnacademy/data4library/tool/BookTools.java
+++ src/main/java/com/nhnacademy/data4library/tool/BookTools.java
@@ -1,7 +1,14 @@
 package com.nhnacademy.data4library.tool;
 
+import com.nhnacademy.data4library.service.BookService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class BookTools {
+
+    private final BookService bookService;
+
+
 }
